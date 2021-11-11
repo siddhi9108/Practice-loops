625ebec64d92904169fe7a2ba3ad908f6d937302
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-11-11 01:39:42 -0600

    Add files via upload

---
+++ Q33Loops.java
@@ -0,0 +1,18 @@
+package com.siddhi.loops1;
+
+import java.util.Scanner;
+
+public class Q33Loops {
+
+	public static void main(String[] args) {
+		// String January, Feb;
+		Scanner ob = new Scanner(System.in);
+		System.out.println("Enter your months: ");
+		String s = ob.next();
+		String Feb;
+		// if (s = Feb) {
+
+		// }
+	}
+
+}
